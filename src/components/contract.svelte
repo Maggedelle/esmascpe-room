8adb57f7af56ae69377e82f7ce93229ec87f96b2
ESMA3000 <kmaszn19@student.aau.dk>
2022-10-03 19:16:13 +0200

    victory screen added

--- src/components/contract.svelte
+++ src/components/contract.svelte
@@ -2,7 +2,8 @@
   export let item;
   export let onClose;
 
-  let pass = ["test"];
+  let pass = ["1234"];
+  //let pass = ["esma vinder beerpong"];
   let passInput = "";
   let showPassContainer = false;
   let cracked = false;
@@ -19,23 +20,37 @@
   }
 </script>
 
-<div class="base">
-  <div class="close" on:click={onClose}>X</div>
-  {#if !showPassContainer}
-    <div class="header">KONTRAKT</div>
-    <div class="baseText">
-      1. I skal være mega kedelige <br />
-      2. I skal være grå ik grøn <br />
-      3. I skal styre jer med alt det papvin <br />
-      4. I må ikke tage næsebajere
+<div>
+  <div class="base">
+    <div class="close" on:click={onClose}>X</div>
+    {#if !showPassContainer}
+      <div class="header">KONTRAKT</div>
+      <div class="baseText">
+        1. I skal være mega kedelige <br />
+        2. I skal være grå ik grøn <br />
+        3. I skal styre jer med alt det papvin <br />
+        4. I må ikke tage næsebajere
+      </div>
+      <button class="button" on:click={() => (showPassContainer = true)}
+        >SLET!!</button
+      >
+    {:else}
+      Skriv Adgangskode
+      <input bind:value={passInput} type="password" />
+      <button class="button" on:click={onPassEnter}>Slet</button>
+    {/if}
+  </div>
+  {#if cracked}
+    <div class="modal-overlay">
+      <div class="modal">
+        <h1 class="victory">Victory</h1>
+        <img src="https://media.tenor.com/vxFNoJHV3I4AAAAC/chiquichico.gif" />
+        <p class="victoryText">
+          Tillykke! Alphabet kontrakten er hermed revet i stykker. ESMA er frie
+          igen!
+        </p>
+      </div>
     </div>
-    <button class="button" on:click={() => (showPassContainer = true)}
-      >SLET!!</button
-    >
-  {:else}
-    Skriv Adgangskode
-    <input bind:value={passInput} type="password" />
-    <button class="button" on:click={onPassEnter}>Slet</button>
   {/if}
 </div>
 
@@ -71,5 +86,39 @@
     min-height: 100px;
     background-color: red;
     font-size: 40px;
+  }
+
+  .modal-overlay {
+    position: fixed;
+    cursor: default;
+    top: -1;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    background-color: transparent;
+  }
+
+  .modal {
+    text-align: center;
+    background-image: linear-gradient(red, orange, yellowgreen, blue);
+    height: 50rem;
+    width: 80rem;
+    border-radius: 20px;
+  }
+
+  .victory {
+    color: yellowgreen;
+    font-weight: bold;
+    font-size: 100px;
+    padding-bottom: 3rem;
+  }
+
+  .victoryText {
+    padding-top: 8rem;
+    color: red;
+    font-weight: bold;
+    font-size: 30px;
   }
 </style>
