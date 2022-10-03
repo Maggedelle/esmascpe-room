7d56af51e0d9a533e16957093d9983023b99de1a
Maggedelle <33657258+Maggedelle@users.noreply.github.com>
2022-10-03 16:57:51 +0200

    Fixed issue where input no longer worked

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,17 @@
 <script>
   import Desktop from "./components/desktop.svelte";
   import Footer from "./components/footer.svelte";
+  import Intro from "./components/intro.svelte";
   import Login from "./components/login.svelte";
   let currentScreen = "login";
 
   let backgroundType = "grey";
 
-  let playedIntro = false;
-  let playingIntro = false;
-  let disableIntro = false;
-
-  function onKeyDown(event) {
-    if (event.keyCode == 13 || event.keyCode == 32) playingIntro = true;
-  }
-
-  function onVideoEnded() {
-    playingIntro = false;
-    playedIntro = true;
-  }
+  let introDone = false;
 </script>
 
-{#if playedIntro == false && playingIntro == false && disableIntro == false}
-  <div class="beforeIntro">
-    <div class="introText">Tryk 'Space' for at begynde</div>
-  </div>
-{:else if playingIntro == true && disableIntro == false}
-  <div class="video-container">
-    <video src="/EsmaIntro.mp4" autoplay={true} on:ended={onVideoEnded} />
-  </div>
+{#if !introDone}
+  <Intro onVideoEnded={() => (introDone = true)} />
 {:else}
   <div class={backgroundType}>
     <div class="base">
@@ -41,7 +25,6 @@
     </div>
   </div>
 {/if}
-<svelte:window on:keydown|preventDefault={onKeyDown} />
 
 <style>
   :root {
@@ -60,48 +43,6 @@
     left: 0;
     height: 100%;
     width: 100%;
-  }
-
-  .beforeIntro {
-    background-color: black;
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .introText {
-    color: white;
-  }
-
-  .video-container {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    background-color: black;
-    overflow-y: hidden;
-  }
-
-  .video-container video {
-    /* Make video to at least 100% wide and tall */
-    min-width: 100vh;
-    min-height: 90vh;
-
-    /* Setting width & height to auto prevents the browser from stretching or squishing the video */
-    width: auto;
-    height: auto;
-
-    /* Center the video */
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
   }
 
   .green {
---
+++ src/components/intro.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  let playedIntro = false;
+  let playingIntro = false;
+  let disableIntro = false;
+
+  export let onVideoEnded;
+  function onKeyDown(event) {
+    if (event.keyCode == 13 || event.keyCode == 32) playingIntro = true;
+  }
+</script>
+
+{#if playedIntro == false && playingIntro == false && disableIntro == false}
+  <div class="beforeIntro">
+    <div class="introText">Tryk 'Space' for at begynde</div>
+  </div>
+{:else if disableIntro == false}
+  <div class="video-container">
+    <video src="/EsmaIntro.mp4" autoplay={true} on:ended={onVideoEnded} />
+  </div>
+{/if}
+
+<svelte:window on:keydown|preventDefault={onKeyDown} />
+
+<style>
+  .beforeIntro {
+    background-color: black;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .introText {
+    color: white;
+  }
+
+  .video-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: black;
+    overflow-y: hidden;
+  }
+
+  .video-container video {
+    /* Make video to at least 100% wide and tall */
+    min-width: 100vh;
+    min-height: 90vh;
+
+    /* Setting width & height to auto prevents the browser from stretching or squishing the video */
+    width: auto;
+    height: auto;
+
+    /* Center the video */
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
